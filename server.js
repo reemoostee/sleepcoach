64b84b29fc2315e8920c0b120e2b0ed95ba0f880
reemoostee <remosteiner@gmail.com>
2021-10-12 12:34:09 +0200

    cleanup

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const PORT = process.env.PORT || 3000;
 
 const app = express();
 
@@ -15,6 +16,6 @@ app.get("/", (req, res) => {
 });
 
 // set port, listen for requests
-app.listen(3000, () => {
+app.listen(PORT, () => {
   console.log("Server is running on port 3000.");
 });
